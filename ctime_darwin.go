669e75ac2d1512375b718859d251e19efb66e071
SnowWarri0r <gerrytranCHINA@gmail.com>
2024-02-20 22:46:09 +0800

    fix: add macos import dependency

--- ctime_darwin.go
+++ ctime_darwin.go
@@ -1,5 +1,10 @@
 package rotw
 
+import (
+	"os"
+	"syscall"
+)
+
 // createTime returns the creation time of a file.
 func createTime(info os.FileInfo) int64 {
 	stat := info.Sys().(*syscall.Stat_t)
